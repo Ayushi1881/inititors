cf54f2e499388a67015559be73a1601fb1bdf63d
pnmsafi <pnmsafi@gmail.com>
2021-10-03 20:15:38 +0530

    second

--- first.js
+++ first.js
@@ -1,3 +1,8 @@
+let adder=require("./second")
+
+console.log(adder(2,3));
+
+
 let word1='Poonam';
 let word2='Safi';
 
@@ -6,10 +11,11 @@ let num1 = 2;
 let num2 = 3;
 
 const FullName = `${num1 + num2} ${word2}`;
-
+let sp=123;
 
 console.log(fullName);
 console.log(FullName);
+console.log(sp);
 const player = {
     name: 'Lebron James',
     club: 'LA Lakers',
@@ -21,6 +27,6 @@ const player = {
   console.log( player.club );
 
 
-  fetch("https://dog.ceo/api/breeds/image/random")
-  .then(response => response.json())
-  .then(data => console.log(data))
+  // fetch("https://dog.ceo/api/breeds/image/random")
+  // .then(response => response.json())
+  // .then(data => console.log(data))
